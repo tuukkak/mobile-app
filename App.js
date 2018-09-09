9c0851cfa204e2a3f3aaf7e4a365dae35e4396e6
Tuukka Koivisto <tuukka.koivisto@wunderdog.fi>
2018-09-09 15:30:01 +0300

    Add redux and axios

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import Navigation from './src/components/content/main/Navigation';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import axios from 'axios';
+import axiosMiddleware from 'redux-axios-middleware';
+import { reducers } from './src/reducers';
+
+const client = axios.create({
+    baseURL: 'https://api.github.com',
+    responseType: 'json'
+});
+
+const store = createStore(reducers, applyMiddleware(axiosMiddleware(client)));
 
 export default class App extends React.Component {
     render() {
-        return <Navigation />;
+        return (
+            <Provider store={store}>
+                <Navigation />
+            </Provider>
+        );
     }
 }
