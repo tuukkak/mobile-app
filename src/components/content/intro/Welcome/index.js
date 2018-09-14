febb7b9ca72c8f6e28941437ccc1c4f4c73ae361
Tuukka Koivisto <tuukka.koivisto@wunderdog.fi>
2018-09-14 22:42:46 +0300

    Code cleaning

--- src/components/content/intro/Welcome/index.js
+++ src/components/content/intro/Welcome/index.js
@@ -11,16 +11,16 @@ export default class Welcome extends React.Component {
     render() {
         return (
             <ImageBackground source={require('images/barren.jpg')} style={{ width: '100%', height: '100%' }}>
-                    <View style={{ flex: 1, justifyContent: 'center' }}>
-                        <Text style={{ textAlign: 'center', color: '#fff' }}>Welcome</Text>
-                    </View>
-                    <View style={{ marginBottom: 50 }}>
-                        <Button title="Let's start" onPress={() => this.props.navigation.navigate('SignUp')} />
-                        <Button
-                            title="I already have an account"
-                            onPress={() => this.props.navigation.navigate('SignIn')}
-                        />
-                    </View>
+                <View style={{ flex: 1, justifyContent: 'center' }}>
+                    <Text style={{ textAlign: 'center', color: '#fff' }}>Welcome</Text>
+                </View>
+                <View style={{ marginBottom: 50 }}>
+                    <Button title="Let's start" onPress={() => this.props.navigation.navigate('SignUp')} />
+                    <Button
+                        title="I already have an account"
+                        onPress={() => this.props.navigation.navigate('SignIn')}
+                    />
+                </View>
             </ImageBackground>
         );
     }
