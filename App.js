ee10f2987f8543e73ca2f510905521cd505a2568
Tuukka Koivisto <tuukka.koivisto@wunderdog.fi>
2018-09-17 19:08:13 +0300

    Code cleaning

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import IntroNavigation from './src/components/content/intro/Navigation';
-import MainNavigation from './src/components/content/main/Navigation';
+import IntroNavigation from 'components/content/intro/Navigation';
+import MainNavigation from 'components/content/main/Navigation';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import axios from 'axios';
